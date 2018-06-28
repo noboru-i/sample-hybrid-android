3a256dd0aac9c88ba395c026808b79cdd3c6cbec
noboru-i <ishikura.noboru@gmail.com>
2018-06-28 11:53:40 +0900

    impl JavascriptInterface

--- app/src/main/java/hm/orz/chaos114/android/samplehybridandroid/MainActivity.java
+++ app/src/main/java/hm/orz/chaos114/android/samplehybridandroid/MainActivity.java
@@ -1,7 +1,12 @@
 package hm.orz.chaos114.android.samplehybridandroid;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new WebAppInterface(this), "MyApp");
+
+        webView.loadUrl("https://noboru-i.github.io/sample-html/webview.html");
+    }
+
+    class WebAppInterface {
+        private Context mContext;
+
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void sendMessage(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
     }
 }
