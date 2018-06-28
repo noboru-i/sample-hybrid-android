ffdf5c4849ed823ceecfb71f683c5ba47d70b734
noboru-i <ishikura.noboru@gmail.com>
2018-06-28 14:50:08 +0900

    handle by host

--- app/src/main/java/hm/orz/chaos114/android/samplehybridandroid/MainActivity.java
+++ app/src/main/java/hm/orz/chaos114/android/samplehybridandroid/MainActivity.java
@@ -1,29 +1,47 @@
 package hm.orz.chaos114.android.samplehybridandroid;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.webview);
+        webView = findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebAppInterface(this), "MyApp");
+        webView.setWebViewClient(new MyWebViewClient());
 
         webView.loadUrl("https://noboru-i.github.io/sample-html/webview.html");
     }
 
-    class WebAppInterface {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        webView.onPause();
+    }
+
+    private class WebAppInterface {
         private Context mContext;
 
         WebAppInterface(Context c) {
@@ -33,6 +51,21 @@ public class MainActivity extends AppCompatActivity {
         @JavascriptInterface
         public void sendMessage(String toast) {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("noboru-i.github.io")) {
+                // in target domain, don't override.
+                return false;
+            }
+
+            // in other domain, execute by other app.
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
         }
     }
 }
